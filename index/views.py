1022a0e82ffe78cca19a065d52fb27b64848c80b
Sarusman <sarusmandu92@gmail.com>
2024-04-23 23:13:51 +0200

    MAJ spammeur ...

--- index/views.py
+++ index/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from .form import ContactForm
 from django.forms.models import model_to_dict
 import requests, json, pytz, datetime
+from datetime import timedelta
 from .models import Formation, Projets
 from . import mail_engine
 
@@ -10,8 +12,20 @@ mailer=mail_engine.Mail_Engine()
 def index(request):
 	formation=[model_to_dict(form) for form in Formation.objects.all()]
 	projets=[model_to_dict(pro) for pro in Projets.objects.all()]
-	mailer.envoyer_email("PORTFOLIO - CONNEXION", "","sarusman.satkunarajah1@gmail.com")
-	return render(request, "index.html", {"formation":formation, "projets":projets})
+	response=render(request, "index.html", {"formation": formation, "projets": projets})
+	print(request.COOKIES)
+	if "abuse" in request.COOKIES:
+		va=int(request.COOKIES["abuse"])
+		if va>5:
+			return redirect("https://www.youtube.com/watch?v=NGGoulSNeIs")
+		else:
+			response.set_cookie('abuse', str(va+1), expires=datetime.datetime.now()+timedelta(hours=3)) # ADAPTATION SERVEUR (TIMEZONE DIFFÉRENT (+2H))
+			mailer.envoyer_email("PORTFOLIO - CONNEXION", "","sarusman.satkunarajah1@gmail.com")
+	else:
+		response.set_cookie('abuse', '0', expires=datetime.datetime.now()+timedelta(hours=3)) # ADAPTATION SERVEUR (TIMEZONE DIFFÉRENT (+2H))
+		mailer.envoyer_email("PORTFOLIO - CONNEXION", "","sarusman.satkunarajah1@gmail.com")
+
+	return response
 
 def checker(request):
 	return HttpResponse("iOBtt5U4fsQBzb2L5SSsIlkVQ1AQ2G9cyJTcorkwbGY.dKu2rqEOe0s1P47nC55iSK0ZHklTZpOnEByxLSVTeZg")
